9dde9590faffa845b1d1c020db5eb6d353d5ee6c
gulomovx <xgulomov447@gmail.com>
2023-12-15 14:22:14 +0500

    updated

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,7 +6,9 @@ import { exploreWorlds } from "../../constants/data";
 import ReactPlayer from "react-player";
 import bg from "../../../public/planet-07.png";
 import hero from "../../../public/stamp.png";
-
+import art from "../../../public/planet-07.png";
+import art2 from "../../../public/planet-08.png";
+import art3 from "../../../public/planet-09.png";
 const About = () => {
   const [data, setData] = useState(exploreWorlds);
 
@@ -20,8 +22,8 @@ const About = () => {
            p-2 animate-bounce"
         />
         {/* content */}
-        <div className=" lg:w-[1400px] w-full mb-96 lg:p-16 p-1  bg-slate-100 border-slate-800 my-2  rounded-lg   ">
-          <div className="w-full">
+        <div className=" lg:w-[1400px] w-full mb-8 lg:p-16 p-1  bg-slate-100 border-slate-800 my-2  rounded-lg   ">
+          <div className="lg:w-[80%] mx-auto py-4">
             <h1 className=" lg:text-xl text-slate-400 my-4">
               New web3 technologies
             </h1>
@@ -33,10 +35,41 @@ const About = () => {
               magni impedit consequatur odit ipsa, error aut voluptas odio vitae
               ad.
             </p>
-            <button className="mt-8 text-white py-2 px-8 bg-blue-800">Try Us Now</button>
+            <button className="mt-8 text-white py-2 px-8 bg-blue-800">
+              Try Us Now
+            </button>
           </div>
         </div>
         {/* content end */}
+        {/* card grid */}
+        <h1 className="lg:text-5xl text-3xl mt-8">Latest Arts</h1>
+        <div className="flex mb-96 max-w-8xl mt-8  flex-col md:flex-row lg:flex-row gap-4  ">
+          <div className="h-full mx-auto">
+            {/* one img */}
+            <Image
+              src={art}
+              alt="art"
+              className="lg:w-[800px] lg:h-[500px] h-[200px] w-[400px] object-cover rounded-2xl"
+            />
+          </div>
+          <div className="flex flex-col   gap-4 justify-between mx-auto">
+            {/* two imges */}
+            <div className="h-full ">
+              <Image
+                src={art2}
+                alt="art"
+                className="w-[400px] h-full  object-cover rounded-2xl"
+              />{" "}
+            </div>
+            <div className="h-full">
+            <Image
+              src={art3}
+              alt='art'
+              className="w-[400px] h-full  object-cover rounded-2xl"
+            />
+            </div>
+          </div>
+        </div>
       </div>
       {/* banner cntent */}
       <div className="max-w-7xl mx-auto">
